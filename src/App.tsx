d24ae662baf8df895b9c6038aaf3e33a3f164f0f
Yves Domingo <yvesrafaeladomingo@gmail.com>
2026-02-06 23:31:31 +0800

    feat: add category selection functionality and update CategoryFilter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
-import CategoryFilter from "./components/CategoryFilter";
-import ProviderFilter from "./components/ProviderFilter";
-import { useState } from "react";
 import type { Category } from "./types/types";
 import { categoryTabs } from "./constants/categories";
+import CategoryFilter from "./components/CategoryFilter";
+import ProviderFilter from "./components/ProviderFilter";
 
 function App() {
   // prettier-ignore
   const [selectedCategory, setSelectedCategory] = useState<Category>(categoryTabs[0]);
+
+  const handleSelectCategory = (category: Category) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <main className="h-screen w-screen">
@@ -17,7 +21,10 @@ function App() {
       <div className="flex flex-col gap-4 px-4">
         <Carousel />
         <ProviderFilter />
-        <CategoryFilter selectedCategory={selectedCategory} />
+        <CategoryFilter
+          selectedCategory={selectedCategory}
+          onSelect={handleSelectCategory}
+        />
       </div>
     </main>
   );
--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -4,18 +4,35 @@ import { categoryTabs } from "../constants/categories";
 interface CategoryFilterItemProps {
   isActive?: boolean;
   category: Category;
+  onSelect: (category: Category) => void;
 }
 
-function CategoryFilterItem({ isActive, category }: CategoryFilterItemProps) {
+function CategoryFilterItem({
+  isActive,
+  category,
+  onSelect,
+}: CategoryFilterItemProps) {
   return (
-    <div className="relative flex h-16 w-15 flex-col items-center justify-center">
+    <div
+      onClick={() => {
+        onSelect(category);
+      }}
+      className="relative flex h-16 w-15 flex-col items-center justify-center hover:cursor-pointer"
+    >
       {isActive && (
         <div className="bg-active-bg absolute top-0 -right-2 rounded px-1 py-0.5">
           <p className="text-[10px] font-bold text-white">8926</p>
         </div>
       )}
 
-      <img className="h-10" src="/category_icons/active/home_active.webp" />
+      <img
+        className="h-10"
+        src={
+          isActive
+            ? `/category_icons/active/${category.value}_active.webp`
+            : `/category_icons/inactive/${category.value}_off.webp`
+        }
+      />
       <p
         className={`${isActive ? "text-primary underline" : "text-txt-secondary"} text-xs whitespace-nowrap uppercase decoration-2 underline-offset-2`}
       >
@@ -27,10 +44,12 @@ function CategoryFilterItem({ isActive, category }: CategoryFilterItemProps) {
 
 interface CategoryFilterProps {
   selectedCategory: Category;
+  onSelect: (category: Category) => void;
 }
 
 export default function CategoryFilter({
   selectedCategory,
+  onSelect,
 }: CategoryFilterProps) {
   return (
     <div className="no-scrollbar flex flex-row overflow-x-auto">
@@ -39,6 +58,7 @@ export default function CategoryFilter({
           <CategoryFilterItem
             isActive={category === selectedCategory}
             category={category}
+            onSelect={onSelect}
           />
         ))}
       </div>
